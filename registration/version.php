c72aefb6a5ebffe5c48601544d5ed1c1d0c5d0b3
Konstantinos Kariyiannis <karugianni@gmail.com>
2023-06-06 00:13:28 +0300

    Add files via upload

---
+++ registration/version.php
@@ -0,0 +1,7 @@
+<? 
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2023060500;
+$plugin->requires  = 2020061500; // Requires Moodle 3.9 or later.
+$plugin->component = 'local_registration';
